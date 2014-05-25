e64f761636c03aa5fc7faa31f29bf72c720f0fa0
songjunyi <astainsong@gmail.com>
2014-05-25 17:01:14 +1000

    Merge branch 'master' of https://github.com/SWEN30006proj/clubbiz
    Conflicts:
    	test.txt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140518142328) do
+ActiveRecord::Schema.define(version: 20140525063059) do
 
   create_table "people", force: true do |t|
     t.text     "name"
@@ -28,5 +28,28 @@ ActiveRecord::Schema.define(version: 20140518142328) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "students", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "school"
+    t.string   "studentid"
+    t.string   "clubs"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true
+  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
 end
