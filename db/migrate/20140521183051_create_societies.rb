7401f60c4f02207c8c032f6e5c819f3024614fe5
costaArmenEdm <costa.armen.edm@gmail.com>
2014-05-25 16:43:19 +1000

    scaffolds for registering society and registering events

---
+++ db/migrate/20140521183051_create_societies.rb
@@ -0,0 +1,25 @@
+class CreateSocieties < ActiveRecord::Migration
+  def
+
+  def change
+
+    create_table :societies do |t|
+      t.string :society_name
+      t.string :registration_number
+      t.string :club_website
+      t.string :club_nickname
+      t.string :password
+      t.string :president_name
+      t.string :president_phone_number
+      t.string :president_email
+      t.string :treasurer_name
+      t.string :treasurer_phone_number
+      t.string :treasurer_email
+      t.string :club_biz_administrator_name
+      t.string :club_biz_administrator_phone_number
+      t.string :club_biz_administrator_email
+
+      t.timestamps
+    end
+  end
+end
